125741e0f4a61bd77287b7ae91fc03c6cb89ed39
Derrick Hammer <derrick@derrickhammer.com>
2024-06-06 15:13:31 -0400

    feat: add etcd db driver

---
+++ db/etcd.go
@@ -0,0 +1,131 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"go.etcd.io/etcd/client/v3"
+	"strings"
+	"time"
+)
+
+var _ KVStore = (*EtcdKVStore)(nil)
+
+const defaultTimeout = 10 * time.Second
+
+// EtcdKVStore is an implementation of the KVStore interface using etcd.
+type EtcdKVStore struct {
+	client     *clientv3.Client
+	prefix     string
+	bucketName string
+	timeout    time.Duration
+	cache      Cache
+}
+
+// NewEtcdKVStore creates a new instance of EtcdKVStore.
+func NewEtcdKVStore(client *clientv3.Client, prefix string, cache Cache, timeout time.Duration) *EtcdKVStore {
+	if timeout == 0 {
+		timeout = defaultTimeout
+	}
+
+	return &EtcdKVStore{
+		client:  client,
+		prefix:  prefix,
+		timeout: timeout,
+		cache:   cache,
+	}
+}
+
+// Open establishes a connection to the etcd cluster.
+func (e *EtcdKVStore) Open() error {
+	// The connection is already established in NewEtcdKVStore,
+	// so this method can be left empty.
+	return nil
+}
+
+// Close closes the connection to the etcd cluster.
+func (e *EtcdKVStore) Close() error {
+	return e.client.Close()
+}
+
+// Get retrieves the value associated with the given key.
+func (e *EtcdKVStore) Get(key []byte) ([]byte, error) {
+	// Check if the value exists in the cache
+	if e.cache != nil {
+		if val, ok := e.cache.Get(key); ok {
+			return val, nil
+		}
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), e.timeout)
+	defer cancel()
+
+	resp, err := e.client.Get(ctx, e.getKey(key))
+	if err != nil {
+		return nil, err
+	}
+	if len(resp.Kvs) == 0 {
+		return nil, nil
+	}
+
+	value := resp.Kvs[0].Value
+	if e.cache != nil {
+		e.cache.Put(key, value)
+	}
+
+	return value, nil
+}
+
+// Put stores the given key-value pair.
+func (e *EtcdKVStore) Put(key []byte, value []byte) error {
+	ctx, cancel := context.WithTimeout(context.Background(), e.timeout)
+	defer cancel()
+
+	_, err := e.client.Put(ctx, e.getKey(key), string(value))
+	if err != nil {
+		return err
+	}
+
+	// Update the cache with the new value
+	if e.cache != nil {
+		e.cache.Put(key, value)
+	}
+
+	return nil
+}
+
+// Delete removes the value associated with the given key.
+func (e *EtcdKVStore) Delete(key []byte) error {
+	ctx, cancel := context.WithTimeout(context.Background(), e.timeout)
+	defer cancel()
+
+	_, err := e.client.Delete(ctx, e.getKey(key))
+	if err != nil {
+		return err
+	}
+
+	// Remove the key from the cache
+	if e.cache != nil {
+		e.cache.Delete(key)
+	}
+
+	return nil
+}
+
+// Bucket creates a new bucket with the given prefix.
+func (e *EtcdKVStore) Bucket(prefix string) (KVStore, error) {
+	if strings.Contains(prefix, "/") {
+		return nil, errors.New("bucket name cannot contain '/'")
+	}
+	return &EtcdKVStore{
+		client:     e.client,
+		prefix:     e.prefix + "/" + prefix,
+		bucketName: prefix,
+		timeout:    e.timeout,
+		cache:      e.cache,
+	}, nil
+}
+
+// getKey constructs the full key path for etcd.
+func (e *EtcdKVStore) getKey(key []byte) string {
+	return e.prefix + "/" + e.bucketName + "/" + string(key)
+}
--- go.mod
+++ go.mod
@@ -14,25 +14,39 @@ require (
 	github.com/multiformats/go-multibase v0.2.0
 	github.com/olebedev/emitter v0.0.0-20230411050614-349169dec2ba
 	github.com/samber/lo v1.39.0
-	github.com/stretchr/testify v1.8.1
+	github.com/stretchr/testify v1.8.4
 	github.com/vmihailenco/msgpack/v5 v5.4.1
 	go.etcd.io/bbolt v1.3.8
+	go.etcd.io/etcd/client/v3 v3.5.14
 	go.uber.org/zap v1.26.0
 	lukechampine.com/blake3 v1.2.1
 	nhooyr.io/websocket v1.7.1
 )
 
 require (
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/mr-tron/base58 v1.1.0 // indirect
 	github.com/multiformats/go-base32 v0.0.3 // indirect
 	github.com/multiformats/go-base36 v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.14 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.14 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
