9367477072b306130c43b06c3cb69fd0ddd642e1
Derrick Hammer <derrick@derrickhammer.com>
2025-02-20 20:31:44 -0500

    wip

---
+++ pkg/protocol/types.go
@@ -0,0 +1,27 @@
+package protocol
+
+type ProtocolMethod int
+
+const (
+	ProtocolMethodHandshakeOpen ProtocolMethod = 0x1
+	ProtocolMethodHandshakeDone ProtocolMethod = 0x2
+	ProtocolMethodSignedMessage ProtocolMethod = 0xA
+	ProtocolMethodHashQuery     ProtocolMethod = 0x4
+	ProtocolMethodAnnouncePeers ProtocolMethod = 0x8
+	ProtocolMethodRegistryQuery ProtocolMethod = 0xD
+	RecordTypeStorageLocation   ProtocolMethod = 0x05
+	RecordTypeStreamEvent       ProtocolMethod = 0x09
+	RecordTypeRegistryEntry     ProtocolMethod = 0x07
+)
+
+var ProtocolMethodMap = map[ProtocolMethod]string{
+	ProtocolMethodHandshakeOpen: "HandshakeOpen",
+	ProtocolMethodHandshakeDone: "IsHandshakeDone",
+	ProtocolMethodSignedMessage: "SignedMessage",
+	ProtocolMethodHashQuery:     "HashQuery",
+	ProtocolMethodAnnouncePeers: "AnnouncePeers",
+	ProtocolMethodRegistryQuery: "RegistryQuery",
+	RecordTypeStorageLocation:   "StorageLocation",
+	RecordTypeStreamEvent:       "StreamEvent",
+	RecordTypeRegistryEntry:     "RegistryEntry",
+}
---
+++ pkg/registry/types.go
@@ -0,0 +1,15 @@
+package registry
+
+type RegistryType int
+
+const (
+	RegistryTypeCID          RegistryType = 0x5a
+	RegistryTypeEncryptedCID RegistryType = 0x5e
+)
+
+var RegistryTypeMap = map[RegistryType]string{
+	RegistryTypeCID:          "CID",
+	RegistryTypeEncryptedCID: "EncryptedCID",
+}
+
+const RegistryMaxDataSize = 64
