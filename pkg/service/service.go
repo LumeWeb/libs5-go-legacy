6171d9eee7366cbaff0a8d52d5b15130775589ac
Derrick Hammer <derrick@derrickhammer.com>
2025-02-21 17:48:24 -0500

    wip

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -2,18 +2,6 @@ package service
 
 import (
 	"context"
-	"go.lumeweb.com/libs5-go/pkg/config"
-	"go.lumeweb.com/libs5-go/pkg/encoding"
-	"go.lumeweb.com/libs5-go/pkg/kv"
-	"go.lumeweb.com/libs5-go/pkg/registry"
-	"go.lumeweb.com/libs5-go/pkg/storage"
-	"go.lumeweb.com/libs5-go/pkg/structs"
-	"go.lumeweb.com/libs5-go/pkg/transport"
-	"go.uber.org/zap"
-	"net/http"
-	"net/url"
-	"old/metadata"
-	"sync"
 )
 
 // BaseService defines the common operations all services must implement
