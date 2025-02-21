742fb41bc2e39a441dc6fe708a60059da7d711da
Derrick Hammer <derrick@derrickhammer.com>
2025-02-20 19:55:22 -0500

    wip

--- libs5.go
+++ libs5.go
@@ -1,18 +1,1 @@
 package libs5_go
-
-// OpenReadFunction is a function that returns a reader for a file or blob
-
-// S5APIProvider defines the interface for interacting with the S5 API
-
-// These are placeholder type definitions that would need to be implemented
-type (
-	BlobCID             []byte
-	Multihash           []byte
-	Route               struct{}
-	SignedRegistryEntry struct{}
-	SignedStreamMessage struct{}
-	KeyPairEd25519      struct {
-		PublicKey  []byte
-		PrivateKey []byte
-	}
-)
